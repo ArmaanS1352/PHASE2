60b46b9292e8c57b3969489f3696803033e49865
Armaan <armaan.s1352@gmail.com>
2025-11-28 19:31:02 -0500

    all maintenance screens now have an associated object class

--- PHASE2/Employee.cs
+++ PHASE2/Employee.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +9,36 @@ namespace PHASE2
 {
     internal class Employee
     {
+        public string EmpId { get; set; }       
+        public string FName { get; set; }       
+        public string? MInit { get; set; }     
+        public string LName { get; set; }      
+        public short? JobId { get; set; }      
+        public byte? JobLvl { get; set; }       
+        public string? PubId { get; set; }      
+        public DateTime? HireDate { get; set; } 
+
+        
+
+        public void Save()
+        {
+            string sql = @"INSERT INTO employee 
+                           (emp_id, fname, minit, lname, job_id, job_lvl, pub_id, hire_date)
+                           VALUES (@emp_id, @fname, @minit, @lname, @job_id, @job_lvl, @pub_id, @hire_date)";
+
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@emp_id", EmpId),
+                new SqlParameter("@fname", FName),
+                new SqlParameter("@minit", DatabaseHelper.DbValue(MInit)),
+                new SqlParameter("@lname", LName),
+                new SqlParameter("@job_id", DatabaseHelper.DbValue(JobId)),
+                new SqlParameter("@job_lvl", DatabaseHelper.DbValue(JobLvl)),
+                new SqlParameter("@pub_id", DatabaseHelper.DbValue(PubId)),
+                new SqlParameter("@hire_date", DatabaseHelper.DbValue(HireDate))
+            };
+
+            DatabaseHelper.ExecuteNonQuery(sql, parameters);
+        }
     }
 }
---
+++ PHASE2/Store.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PHASE2
+{
+    internal class Store
+    {
+        public string StorId { get; set; }        
+        public string? StorName { get; set; }     
+        public string? StorAddress { get; set; }  
+        public string? City { get; set; }         
+        public string? State { get; set; }        
+        public string? Zip { get; set; }
+
+
+
+
+        public void Save()
+        {
+            string sql = @"INSERT INTO stores (stor_id, stor_name, stor_address, city, state, zip)
+                           VALUES (@id, @name, @address, @city, @state, @zip)";
+
+            SqlParameter[] parameters =
+            {
+                new SqlParameter("@id", StorId),
+                new SqlParameter("@name", DatabaseHelper.DbValue(StorName)),
+                new SqlParameter("@address", DatabaseHelper.DbValue(StorAddress)),
+                new SqlParameter("@city", DatabaseHelper.DbValue(City)),
+                new SqlParameter("@state", DatabaseHelper.DbValue(State)),
+                new SqlParameter("@zip", DatabaseHelper.DbValue(Zip))
+            };
+
+            DatabaseHelper.ExecuteNonQuery(sql, parameters);
+        }
+    }
+}
